d49b4ed34b9937cc1372df2bce78f19fcb3568a1
Pavlo <pavlo@kerestey.net>
2012-03-19 16:50:57 +0100

    commit forgotten files :)

--- src/Da/Http.hs
+++ src/Da/Http.hs
@@ -5,21 +5,29 @@ import Control.Applicative ((<$>))
 import Data.List (filter)
 import Data.Text (Text(..), pack, unpack)
 import Data.Text.Encoding
-import Network.Curl.Download (openURIString)
+import Network.HTTP
+import Network.HTTP.Base (Response(..))
+import Network.HTTP.Headers (Header(..))
+import Network.Stream (Result)
 import Text.HTML.TagSoup
 
 fetchLinks :: Text -> IO [Tag Text]
-fetchLinks url = (filterLinksOnly . parseTags) <$> openAsTags url
+fetchLinks url = (filterLinksOnly . parseTags) <$> fetchPage url
+
+fetchPage :: Text -> IO Text
+fetchPage url = simpleHTTP (getRequest (unpack url)) >>= fetchBody
+
+fetchBody :: (Result (Response String)) -> IO Text
+fetchBody res = case lookupHeader HdrContentType (headers res) of
+  Just "text/html" -> pack <$> getResponseBody res
+  otherwise        -> return ""
+  where
+    headers :: (Result (Response String)) -> [Header]
+    headers (Right rsp) = rspHeaders rsp
+    headers _           = []
 
 filterLinksOnly :: [Tag Text] -> [Tag Text]
 filterLinksOnly tags = filter isLink tags
   where
     isLink (TagOpen "a" _) = True
     isLink _               = False
-
-openAsTags :: Text -> IO Text
-openAsTags s = handleDownload <$> openURIString (unpack s)
-  where
-    handleDownload :: (Either String String) -> Text
-    handleDownload (Left _) = ""
-    handleDownload (Right content) = (pack content)
