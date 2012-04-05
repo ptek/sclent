a39e8db27b5a28520dccf7f4891e2973d8d37fac
Pavlo <pavlo@kerestey.net>
2012-04-05 15:31:28 +0200

    rewrite the service to run in an enumerator pattern. Hopefully it will make the memory footprint more controllable

--- src/Service/CrawlerService.hs
+++ src/Service/CrawlerService.hs
@@ -1,18 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Service.CrawlerService (
   runCrawler
 ) where
 
 import Control.Applicative ((<$>))
+import Control.Monad.IO.Class
+import Data.Enumerator (Iteratee(..), ($$), (>==>), run_, enumList)
+import qualified Data.Enumerator.List as EL
 import Data.List ((\\),nub)
 import Data.Text (Text(..))
 import Text.HTML.TagSoup (Tag(..))
 import Model.Crawler
 
 runCrawler :: (Text -> IO [Tag Text]) -> Text -> [Text] -> IO [(Text, Bool)]
-runCrawler fetcher domainToSearchFor domainsToSearchOn = mapM search domainsToSearchOn where
-  search d = do
-    found <- crawl fetcher domainToSearchFor d [d] [] 3
-    return (d, found)
+runCrawler fetcher dFor dOn = run_ (enumList 1 dOn $$ search) where
+  search :: Iteratee Text IO [(Text, Bool)]
+  search = do
+    maybeDomain <- EL.head
+    case maybeDomain of
+      Nothing -> return []
+      Just d -> do
+        rest <- search
+        found <- liftIO (crawl fetcher dFor d [d] [] 3)
+        return ([(d,found)] ++ rest)
 
 crawl :: (Text -> IO [Tag Text]) -> Text -> Text -> [Text] -> [Text] -> Int -> IO Bool
 crawl _       _      _      _       _   0     = return False
